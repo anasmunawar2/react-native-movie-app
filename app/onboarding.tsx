e985de14aaf776717a487148201d3e7ab9dbb933
anasmunawar2 <anasmunawar2@gmail.com>
2025-04-04 12:25:13 +0500

    routung

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Inboarding</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Onboarding;
